256032af101843b613be4dffc61c3508a1788a1c
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-08-19 19:15:40 -0400

    Added functional health bars to all enemies

--- core/src/com/nishimura/cholessthanthree/actors/Bird.kt
+++ core/src/com/nishimura/cholessthanthree/actors/Bird.kt
@@ -4,36 +4,38 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.nishimura.cholessthanthree.Assets
 import com.nishimura.cholessthanthree.MyGdxGame
+import com.nishimura.cholessthanthree.PlayerState
+import kotlin.properties.Delegates
 
-class Bird : Enemy() {
+class Bird : Enemy(Image(Assets.atlasSkin.getDrawable(Assets.bird)).apply{
+    color = Color.BLACK
+    setSize(MyGdxGame.WIDTH * 0.2f, MyGdxGame.WIDTH * 0.05f)
+    setOrigin(width / 2, height / 2)
+}) {
     override var maxHealth = 50
-    override var currentHealth = 50
+    override var currentHealth by Delegates.observable(maxHealth, { property, oldValue, newValue ->
+        if(newValue <= 0) {
+            clearActions()
+            clearListeners()
+            PlayerState.targetableEntities.remove(this)
+            addAction(Actions.sequence(
+                    Actions.parallel(Actions.moveTo(MyGdxGame.WIDTH, MyGdxGame.HEIGHT, 1.5f,
+                            Interpolation.swingIn),
+                            Actions.rotateBy(3600f, 1.5f)),
+                    Actions.removeActor()))
+        }
+    })
     override var isDead = false
     init {
-        color = Color.BLACK
         val flap = Actions.forever(
                 Actions.sequence(Actions.moveBy(0f, -50f, 1.5f, Interpolation.exp5),
                         Actions.moveBy(0f, 50f, 1.5f)))
-        this.setDrawable(Assets.atlasSkin.getDrawable(Assets.bird))
-        setSize(MyGdxGame.WIDTH * 0.2f, MyGdxGame.WIDTH * 0.05f)
-        setPosition(MyGdxGame.WIDTH * 0.8f - width, MyGdxGame.HEIGHT * 0.6f)
-        setOrigin(width / 2, height / 2)
-        addListener(object : ClickListener() {
-            override fun clicked(event: InputEvent?, x: Float, y: Float) {
-                super.clicked(event, x, y)
-                removeAction(flap)
-                addAction(Actions.sequence(
-                        Actions.parallel(Actions.moveTo(MyGdxGame.WIDTH, MyGdxGame.HEIGHT, 1.5f,
-                                Interpolation.swingIn),
-                                Actions.rotateBy(3600f, 1.5f)),
-                        Actions.removeActor()
-                ))
-            }
-        })
-        addAction(flap)
 
+        addAction(flap)
+        setPosition(MyGdxGame.WIDTH * 0.8f - width, MyGdxGame.HEIGHT * 0.6f)
     }
 }
--- core/src/com/nishimura/cholessthanthree/actors/Enemy.kt
+++ core/src/com/nishimura/cholessthanthree/actors/Enemy.kt
@@ -1,28 +1,72 @@
 package com.nishimura.cholessthanthree.actors
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.nishimura.cholessthanthree.Assets
 import com.nishimura.cholessthanthree.Damageable
 import com.nishimura.cholessthanthree.MyGdxGame
 import com.nishimura.cholessthanthree.Targetable
 
-abstract class Enemy: Image(), Targetable, Damageable {
+abstract class Enemy(val image: Image) : Group(), Targetable, Damageable {
+    companion object {
+        val healthBarWidth = MyGdxGame.WIDTH * 0.05f
+        val healthBarHeight = healthBarWidth / 4
+    }
+
+    init {
+        this.setSize(image.width, image.height + healthBarHeight)
+        //Leave room for health bar below
+        image.setPosition(0f, healthBarHeight)
+        this.addActor(image)
+    }
     override var block = 0
     override fun hit(x: Float, y: Float): Boolean {
-        return  (x >= this.x && x < width+this.x && y >= this.y && y < height+this.y)
+        return (x >= this.x && x < width + this.x && y >= this.y && y < height + this.y)
     }
 
     override fun getTargetX(): Float {
-        return x+width/2
+        return x + width / 2
     }
 
     override fun getTargetY(): Float {
-        return y+height/2
+        return y + height / 2
+    }
+
+    val healthBarBack = Image(Assets.atlasSkin.getRegion("health_empty")).apply {
+        setPosition(width / 2 - healthBarWidth / 2, 0f)
+        setSize(healthBarWidth, healthBarHeight)
+        addActor(this)
+    }
+    val healthBarFront = Image(Assets.atlasSkin.getRegion("health_full")).apply {
+        setPosition(width / 2 - healthBarWidth / 2, 0f)
+        setSize(healthBarWidth, healthBarHeight)
+        addActor(this)
+    }
+
+
+
+    final override fun addActor(actor: Actor?) {
+        super.addActor(actor)
+    }
+
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        batch?.color = Color.WHITE
+        super.draw(batch, parentAlpha)
+        if (currentHealth > 0) {
+            if (this.clipBegin(x+healthBarFront.x, y + healthBarFront.y, healthBarWidth * currentHealth / maxHealth, healthBarFront.height)) {
+                children.filterNot { it == healthBarFront }.forEach { it.isVisible = false }
+                super.draw(batch, parentAlpha)
+                children.filterNot { it == healthBarFront }.forEach { it.isVisible = true }
+                clipEnd()
+            } else {
+                println("clip fail")
+            }
+        }
+
     }
 
 }
